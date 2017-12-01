0c52b90b919bc9f4b438e034b5fca74df32d284b
jeikerxiao <jeiker@126.com>
2017-12-01 14:10:04 +0800

    add simple project
    add simple project

---
+++ web1-simple/src/main/java/com/jeiker/web/HelloServlet.java
@@ -0,0 +1,29 @@
+package com.jeiker.web;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * @author : xiao
+ * @date : 17/12/1 下午1:37
+ * @description :
+ */
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet{
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String currentTime = dateFormat.format(new Date());
+
+        request.setAttribute("currentTime", currentTime);
+        request.getRequestDispatcher("/WEB-INF/jsp/hello.jsp").forward(request, response);
+    }
+}
---
+++ web1-simple/src/main/webapp/WEB-INF/jsp/hello.jsp
@@ -0,0 +1,19 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: xiao
+  Date: 17/12/1
+  Time: 下午1:44
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page pageEncoding="UTF-8" %>
+<html>
+<head>
+    <title>Hello</title>
+</head>
+<body>
+
+<h1>Hello!</h1>
+<h2>当前时间：${currentTime}</h2>
+
+</body>
+</html>
